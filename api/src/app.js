fafdceb4de1b777e2b57ab5b44b7d30089706131
mrchurches <laureanoiglesias34@gmail.com>
2022-11-03 02:09:07 -0300

    u a

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const {URl_ALLOWED} = process.env;
+const {URL_ALLOWED} = process.env;
 require('./db.js');
 
 const server = express();
