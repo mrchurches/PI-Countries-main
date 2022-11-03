334dd490288c2bed59b1c9f2ad36b86d4eaed3df
mrchurches <laureanoiglesias34@gmail.com>
2022-11-02 21:36:07 -0300

    a lot of changes

--- client/src/components/landing/landing.jsx
+++ client/src/components/landing/landing.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
-import "./landing.css"
-export default function Landing(){
-    return(
-        <div className="landing">
-            <h1>The countries page</h1>
-            <Link to="/home" className="link">
+import style from "./landing.module.css";
+import world from "../../images/world.png"
+export default function Landing() {
+  return (
+    <div className={style.landing}>
+            <div className={style.info}>
+                <p>the whole</p>
+                <h3>WORLD</h3>
+                <p>Explore all the countries around the world and his information</p>
+            </div>
+            <div className={style.enter}>
+                <img className="App-logo" src={world} alt="world" />
+                <Link to="/home" className={style.link}>
                 <h4>Enter</h4>
-            </Link>
-        </div>
-    )
+                </Link>
+            </div>
+    </div>
+  );
 }
--- client/src/components/nav_bar/nav_bar.jsx
+++ client/src/components/nav_bar/nav_bar.jsx
@@ -1,4 +1,4 @@
-import "./nav_bar.css"
+import style from "./nav_bar.module.css"
 import { Link, useLocation} from "react-router-dom";
 import logo from "../../images/world.png"
 import SearchBar from "../search_bar/search_bar";
@@ -6,20 +6,20 @@ import SearchBar from "../search_bar/search_bar";
 export default function NavBar(){
     let location = useLocation();
     return(
-        <div className="navbar">
-            <div className="first">
-                <Link className="buttons" to="/">
-                <img src={logo} alt="world" className="logo"/>
-                <span>The countries page</span>
+        <div className={style.navbar}>
+            <div className={style.first}>
+                <Link className={style.hvr_underline_from_center} to="/">
+                <img src={logo} alt="world" className={style.logo}/>
+                <span>World</span>
                 </Link>
             </div>
-            <div className="div">
-                <Link className="buttons" to="/home">
+            <div>
+                <Link className={style.hvr_underline_from_center} to="/home">
                     <div>Home</div>
                 </Link>
             </div>
-            <div className="div">
-                <Link className="buttons" to="/country/create">
+            <div>
+                <Link className={style.hvr_underline_from_center} to="/country/create">
                     <div>Create activity</div>
                 </Link>
             </div>
