ed7bf817b5c5e660996030235ab975dfc1f9a887
mrchurches <laureanoiglesias34@gmail.com>
2022-11-03 02:03:29 -0300

    url allowed

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', URl_ALLOWED);
+  res.header('Access-Control-Allow-Origin', URL_ALLOWED);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
